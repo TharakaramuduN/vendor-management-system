b27c4c5ba5f0f8cfb7f183f2c0e1d972dac6162d
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-05-04 15:50:26 +0530

    create vendor and purchase order models

--- vendor_app/models.py
+++ vendor_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Vendor(models.Model):
+    vendor_code = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=250)
+    contact_details = models.TextField()
+    address = models.TextField()
+    on_time_delivery_rate = models.FloatField(default=0)
+    quality_rating_avg = models.FloatField(default=0)
+    average_response_time = models.FloatField(default=0)
+    fulfillment_rate = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class PurchaseOrder(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('canceled', 'Canceled'),
+    )
+
+    po_number = models.CharField(max_length=50)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField(auto_now_add=True)
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"PO {self.po_number} - {self.vendor.name}"
