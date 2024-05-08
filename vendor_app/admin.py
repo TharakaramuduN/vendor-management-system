102cf1ed28c07ddce8cca4e377711bc053aac46b
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-05-08 16:36:54 +0530

    register models in admin in vendor_app and vendor_performance

--- vendor_app/admin.py
+++ vendor_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Vendor, PurchaseOrder
 
 # Register your models here.
+
+admin.site.register(Vendor)
+
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    readonly_fields = ['issue_date']
+
+admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
