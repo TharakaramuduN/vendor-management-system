31afcfe1b06647275495d6e154ecb82052d98fae
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-05-08 16:35:25 +0530

    add generic views in vendor_performance views

--- vendor_performance/views.py
+++ vendor_performance/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+from .models import HistoricalPerformance
+from .serializers import HistoricalPerformanceSerializer
 
 # Create your views here.
+
+class VendorPerformanceListAPIView(RetrieveAPIView):
+    queryset = HistoricalPerformance.objects.all()
+    serializer_class = HistoricalPerformanceSerializer
