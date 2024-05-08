449feb42917892448ae52f7327bb640f74f9130c
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-05-08 16:20:43 +0530

    create vendor code and po_number on create method

--- vendor_app/serializers.py
+++ vendor_app/serializers.py
@@ -1,12 +1,24 @@
 from .models import Vendor, PurchaseOrder
 from rest_framework import serializers
+import uuid
+from django.utils import timezone
 
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vendor
         fields = '__all__'
+        read_only_fields = ['vendor_code','quality_rating_avg','average_response_time','fulfillment_rate','on_time_delivery_rate']
+    
+    def create(self, validated_data):
+        validated_data['vendor_code'] = uuid.uuid4()
+        return super().create(validated_data)
 
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PurchaseOrder
         fields = '__all__'
+        read_only_fields = ['po_number']
+    
+    def create(self, validated_data):
+        validated_data['po_number'] = uuid.uuid4()
+        return super().create(validated_data)
