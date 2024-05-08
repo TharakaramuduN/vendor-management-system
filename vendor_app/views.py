2c6dfc710b706a9cc5b8be8164f142af83ec3bf6
Tharakaramudu Nagaladinne <tharakaramudu.nagaladinne@gmail.com>
2024-05-08 16:33:18 +0530

    add generic views in vendor_app views

--- vendor_app/views.py
+++ vendor_app/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
+from .models import Vendor, PurchaseOrder
+from .serializers import VendorSerializer, PurchaseOrderSerializer
+from rest_framework.response import Response
+from django.utils import timezone
+from rest_framework import status
+from datetime import timedelta
 
 # Create your views here.
+
+class VendorListCreateAPIView(ListCreateAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+class VendorRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    
+
+class PurchaseOrderListCreateAPIView(ListCreateAPIView):
+    serializer_class = PurchaseOrderSerializer
+
+    def get_queryset(self):
+        queryset = PurchaseOrder.objects.all()
+        vendor_id = self.request.query_params.get('vendor_id')
+        if vendor_id:
+            queryset = queryset.filter(vendor=vendor_id)
+        return queryset
+
+class PurchaseOrderRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+    def perform_update(self, serializer):
+        if serializer.validated_data.get('status') == 'completed':
+            serializer.validated_data['delivery_date'] = timezone.now()
+        return super().perform_update(serializer)
+    
+class AcknowledgmentDateUpdateAPIView(UpdateAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.acknowledgment_date = timezone.now()
+        instance.expected_delivery_date = instance.acknowledgment_date + timedelta(days=7)
+        instance.save(update_fields = ['acknowledgment_date','expected_delivery_date'])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
